1844a8ff4e2681f6a7c7f7834a8ba3f74da9085a
patilpriyanka2325 <patilpriyanka2325@gmail.com>
2024-01-12 14:38:03 +0530

    Add project files.

---
+++ Calculator/Calculation.cs
@@ -0,0 +1,102 @@
+﻿namespace Calculator
+{
+    internal class Calculation
+    {
+        public double Calculate(double number1, double number2, string operation)
+        {
+
+            string nonNullOperation = operation ?? throw new ArgumentNullException(nameof(operation));
+            double answer;
+
+            switch (operation)
+            {
+                case "+":
+                    answer = Add(number1, number2);
+                    break;
+
+                case "-":
+                    answer = Minus(number1, number2);
+                    break;
+
+                case "*":
+                    answer = Multiply(number1, number2);
+                    break;
+
+                case "/":
+                    answer = Divide(number1, number2);
+                    break;
+
+                default:
+                    answer = 0;
+                    break;
+            }
+            return answer;
+
+            //if (operation == "/")
+            //{
+            //    return Divide(number1, number2);
+            //}
+            //else
+            //{
+            //    throw new CalculationOperationNotSupportedException(operation);
+
+            //}
+        }
+        private double Add(double number, double anotherNumber)
+        {
+            try
+            {
+                var result = number + anotherNumber;
+                return result;
+            }
+            catch (ArithmeticException ex)
+            {
+                throw new /*CalculationException*/ Exception("An error occured during division", ex);
+            }
+
+        }
+
+        private double Minus(double number, double anotherNumber)
+        {
+            try
+            {
+                var result = number - anotherNumber;
+                return result;
+            }
+            catch (ArithmeticException ex)
+            {
+                throw new /*CalculationException*/ Exception("An error occured during division", ex);
+            }
+
+        }
+
+        private double Multiply(double number, double anotherNumber)
+        {
+            try
+            {
+                var result = number * anotherNumber;
+                return result;
+            }
+            catch (ArithmeticException ex)
+            {
+                throw new /*CalculationException*/ Exception("An error occured during division", ex);
+            }
+
+        }
+
+        private double Divide(double number, double divisor)
+        {
+            try
+            {
+                var result = number / divisor;
+                return result;
+            }
+            catch (ArithmeticException ex)
+            {
+                throw new /*CalculationException*/ Exception("An error occured during division", ex);
+            }
+
+        }
+
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,46 @@
+﻿using static System.Console;
+
+namespace Calculator
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            WriteLine("Enter first number");
+            var number1 = Convert.ToDouble(ReadLine());
+
+            WriteLine("Enter second number");
+            var number2 = double.Parse(ReadLine());
+
+            WriteLine("Enter operation");
+            string operation = ReadLine().ToUpperInvariant();
+
+
+            var calculator = new Calculation();
+            try
+            {
+                var result = calculator.Calculate(number1, number2, operation);
+                DisplayResult(result);
+            }
+
+            catch (/*CalculationException*/ Exception ex)
+            {
+                //Log.Error;
+                WriteLine(ex);
+            }
+            finally
+            {
+                WriteLine("...Finally...");
+            }
+
+
+            WriteLine("\nPress enter to exit.");
+            ReadLine();
+        }
+
+        private static void DisplayResult(double result)
+        {
+            WriteLine($"Result is: {result}");
+        }
+    }
+}
